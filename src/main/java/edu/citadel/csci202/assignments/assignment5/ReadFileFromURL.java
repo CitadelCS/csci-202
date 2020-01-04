4b14fe7691150748012871ff2f60eb924aab17f1
harvest <wottinger@trhc.com>
2020-01-04 11:33:21 -0500

    Updated assignment 5

--- src/main/java/edu/citadel/csci202/assignments/assignment5/ReadFileFromURL.java
+++ src/main/java/edu/citadel/csci202/assignments/assignment5/ReadFileFromURL.java
@@ -1,42 +1,29 @@
 package edu.citadel.csci202.assignments.assignment5;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
 import java.io.IOException;
 import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.Scanner;
 
 public class ReadFileFromURL {
 
-    // sample web page = https://en.wikipedia.org/wiki/Citadel
-
     public static void main(String[] args) {
-        System.out.print("Enter a URL: ");
-        String URLString = new Scanner(System.in).next();
+
+        // Read the JSoup Cookbook to learn more about how to the library
+        // https://jsoup.org/cookbook/
 
         try {
-            URL url = new URL(URLString);
-            CharSequence cs = "<h1 id=\"firstHeading\" class=\"firstHeading\" lang=\"en\">";
-            Scanner input = new Scanner(url.openStream());
+            String url = "https://en.wikipedia.org/wiki/Citadel";
+            Document document = Jsoup.connect(url).get();
 
-            while (input.hasNext()) {
-                String line = input.nextLine();
-                if(line.contains(cs)){
-                    int start = line.indexOf('>');
-                    int end = line.indexOf('<', start);
-                    System.out.println(line.substring(start+1, end));
+            // Get heading value
+            String heading = document.getElementById("firstHeading").text();
 
-
-                    //System.out.println(line);
-                }
-
-            }
-
-
-        }
-        catch (MalformedURLException ex) {
+            //TODO: Finish the program
+        } catch (MalformedURLException ex) {
             System.out.println("Invalid URL");
-        }
-        catch (IOException ex) {
+        } catch (IOException ex) {
             System.out.println("IO Errors");
         }
     }
